9ad27cac2e5f08ac607a7be8db97589d414b0028
dheerapat <dheerapat.t@kkumail.com>
2020-03-07 17:11:39 +0700

    update add new album

--- portfolio.py
+++ portfolio.py
@@ -21,6 +21,12 @@ kaewls = ['kaewleft/' + file for file in kaewls]
 kaewrs = os.listdir('static/album/kaew/kaewright')
 kaewrs = ['kaewright/' + file for file in kaewrs]
 
+gradrs = os.listdir('static/album/graduate/right')
+gradrs = ['right/' + file for file in gradrs]
+
+gradls = os.listdir('static/album/graduate/left')
+gradls = ['left/' + file for file in gradls]
+
 @app.route('/')
 def index():
 	imgrand = random.sample(imgs,k=5)
@@ -42,6 +48,10 @@ def nuna():
 @app.route('/learn')
 def learn():
 	return render_template('learn.html')
+
+@app.route('/graduation')
+def graduation():
+	return render_template('graduation.html', gradrs=gradrs, gradls=gradls)
 
 if __name__=='__main__':
 	app.run(debug=True)
